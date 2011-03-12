1f4e16b97055abee3539daceea5a75fdf6de924e
Shannon Wynter <bukkit-bedrock.git.alias@fremnet.net>
2011-03-13 04:28:49 +1000

    Added yml file back to build.xml Fixed brain dead logic for replacing blocks (extra sleep + caffine + food + alcohol made this possible)

--- src/au/net/fremnet/bukkit/Bedrock/BedrockPlayerListener.java
+++ src/au/net/fremnet/bukkit/Bedrock/BedrockPlayerListener.java
@@ -12,29 +12,49 @@ public class BedrockPlayerListener extends PlayerListener {
 		World world = event.getTo().getWorld();
 		Location from = event.getFrom();
 		Location to = event.getTo();
-		int startX, startZ;
-		int endX, endZ;
 
+		System.out.println(String.format("Got movement %d: %d,%d > %d,%d",
+			to.getBlockY(),
+			to.getBlockX(),
+			to.getBlockZ(),
+			from.getBlockX(),
+			from.getBlockZ()
+		));
+
+		
 		// Don't bother checking below this height
-		if (to.getBlockY() > Bedrock.CheckBelow) return;
+		if (to.getBlockY() - 1 > Bedrock.CheckBelow) return;
 
 		// Work out the range to check
-		if (from.getBlockY() > Bedrock.CheckBelow) {
+		if (from.getBlockY() - 1 > Bedrock.CheckBelow) {
 			// if player is just stepping below the CheckBelow threshold, check
 			// everything
-			startX = to.getBlockX() - Bedrock.FlattenSquare;
-			endX = to.getBlockX() + Bedrock.FlattenSquare;
-			startZ = to.getBlockY() - Bedrock.FlattenSquare;
-			endZ = to.getBlockY() + Bedrock.FlattenSquare;
+			int startX = to.getBlockX() - Bedrock.FlattenSquare;
+			int endX = to.getBlockX() + Bedrock.FlattenSquare;
+			int startZ = to.getBlockZ() - Bedrock.FlattenSquare;
+			int endZ = to.getBlockZ() + Bedrock.FlattenSquare;
+			replace(world, startX, startZ, endX, endZ);
+
 		}
 		else {
-			// Otherwise check just the blocks that'll now be in range
-			startX = Math.min(to.getBlockX(), from.getBlockX()) - Bedrock.FlattenSquare;
-			endX = Math.max(to.getBlockX(), from.getBlockX()) + Bedrock.FlattenSquare;
-			startZ = Math.min(to.getBlockZ(), from.getBlockZ()) - Bedrock.FlattenSquare;
-			endZ = Math.max(to.getBlockZ(), from.getBlockZ()) + Bedrock.FlattenSquare;
+			if (to.getBlockX() != from.getBlockX()) {
+				int startX = source(from.getBlockX(), to.getBlockX(), false);
+				int endX = source(from.getBlockX(), to.getBlockX(), true);
+				int startZ = source(from.getBlockZ(), to.getBlockZ(), false);
+				int endZ = startZ + Bedrock.FlattenSquare * 2;
+				replace(world, startX, startZ, endX, endZ);
+			}
+			if (to.getBlockZ() != from.getBlockZ()) {
+				int startX = source(from.getBlockX(), to.getBlockX(), false);
+				int endX = startX + Bedrock.FlattenSquare * 2;
+				int startZ = source(from.getBlockZ(), to.getBlockZ(), false);
+				int endZ = source(from.getBlockZ(), to.getBlockZ(), true);
+				replace(world, startX, startZ, endX, endZ);
+			}
 		}
+	}
 
+	private void replace(World world, int startX, int startZ, int endX, int endZ) {
 		for (int x = startX; x <= endX; x++)
 			for (int z = startZ; z <= endZ; z++) {
 				// Blocks above level 0
@@ -43,5 +63,20 @@ public class BedrockPlayerListener extends PlayerListener {
 						world.getBlockAt(x, y, z).setType(Bedrock.weightedMaterialPicker.get());
 				if (world.getBlockAt(x, 0, z).getType() != Material.BEDROCK) world.getBlockAt(x, 0, z).setType(Material.BEDROCK);
 			}
+	}
+	
+	private int source(int from, int to, boolean inv) {
+		if (from > to) {
+			if (inv) {
+				return from - Bedrock.FlattenSquare;
+			}
+			else {
+				return to - Bedrock.FlattenSquare;
+			}
+		}
+		if (inv) {
+			return from + Bedrock.FlattenSquare;
+		}
+		return to + Bedrock.FlattenSquare;
 	}
 }
