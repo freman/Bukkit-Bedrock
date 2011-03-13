ef5e996fafee4808e4d409ac315c233ddf5130cb
Shannon Wynter <bukkit-bedrock.git.alias@fremnet.net>
2011-03-13 11:56:27 +1000

    Switched to static hashmap from simple boolean

--- src/au/net/fremnet/libs/bukkit/Copyright.java
+++ src/au/net/fremnet/libs/bukkit/Copyright.java
@@ -2,12 +2,13 @@ package au.net.fremnet.libs.bukkit;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Copyright {
-	private static boolean shown = false;
+public class Copyright { 
+	private static HashMap<String, Boolean> shown = new HashMap<String, Boolean>();
 	
 	public static void show(JavaPlugin plugin, String copyright) {
 		show(plugin, copyright, false);
@@ -15,7 +16,8 @@ public class Copyright {
 	
 	public static void show(JavaPlugin plugin, String copyright, Boolean gpl) {
 		PluginDescriptionFile pd = plugin.getDescription();
-		if (!shown) {
+		String className = plugin.getClass().getSimpleName();
+		if (!shown.containsKey(className)) {
 			String out = pd.getName() + " - Copyright " + copyright;
 			if (gpl) {
 				out += " - Source released under GPL3";
@@ -28,7 +30,7 @@ public class Copyright {
 			
 			try {
 				File jarFile = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
-				if (!jarFile.getName().equalsIgnoreCase(plugin.getClass().getSimpleName() + ".jar")) {
+				if (!jarFile.getName().equalsIgnoreCase(className + ".jar")) {
 					plugin.getServer().getLogger().warning(pd.getName() + ": Plugin may not be authentic!");
 				}
 			}
@@ -37,7 +39,7 @@ public class Copyright {
 				e.printStackTrace();
 			}
 			
-			shown = true;
+			shown.put(className, true);
 		}
 	}
 }
